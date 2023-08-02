32f3ab44cee379bf92dff45f4dbadf405482ca4b
Ruben <ruben@Mamikons-iMac-2.local>
2023-08-02 13:51:06 +0400

    t

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-    echo "test";
+    $con = mysqli_connect("49.12.164.154", "wamxcmwl_r", "44023588Rx");
+
+    if($con->connect_error) {
+        echo "error";
+    }
+    else {
+        echo "ok";
+    }
     ?>
